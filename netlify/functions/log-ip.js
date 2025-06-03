884a703804512f83c99346b0ee79dbc2e2947dc7
ImMuhammedKsa <muh_tamin@hotmail.com>
2025-06-03 05:28:01 +0300

    Update log-ip.js

--- netlify/functions/log-ip.js
+++ netlify/functions/log-ip.js
@@ -1,5 +1,3 @@
-const fetch = require('node-fetch');
-
 exports.handler = async (event, context) => {
   const webhookURL = "https://discord.com/api/webhooks/1379270305272299530/EdXC7ENWy6IzQiR1-ETKyeyitOmG_It2088qG3SROttMnLqzvp6YV879Wqs-WwIYa4yk";
 
